88dd5d3579e26c8fbdcba48f84cb0a5aa28d56e3
ayyazbinmumtaz3 <ayyazbinmumtaz3@yahoo.com>
2024-05-24 12:47:08 +0500

    create backend and integrate database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 app.use(express.json());
 
 app.use(
@@ -9,6 +18,25 @@ app.use(
     origin: "*",
   })
 );
+
+app.post("./create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.json({ data: "Hello World" });
