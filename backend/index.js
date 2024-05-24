532f1fe62c67415354e6e8a4b184f42cc0f945ce
ayyazbinmumtaz3 <ayyazbinmumtaz3@yahoo.com>
2024-05-24 16:26:03 +0500

    create account and login integate

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
-
+const User = require("./models/user.model");
 const config = require("./config.json");
 const mongoose = require("mongoose");
-
-mongoose.connect(config.connectionString);
-
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const jwt = require("jsonwebtoken");
-const { authenticateToken } = require("./utilities");
+mongoose.connect(config.connectionString);
+
 app.use(express.json());
 
 app.use(
@@ -19,7 +18,11 @@ app.use(
   })
 );
 
-app.post("./create-account", async (req, res) => {
+app.get("/", (req, res) => {
+  res.json({ data: "Hello World" });
+});
+
+app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
   if (!fullName) {
     return res
@@ -36,10 +39,67 @@ app.post("./create-account", async (req, res) => {
       .status(400)
       .json({ error: true, message: "Password is required" });
   }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "User already exists",
+    });
+  }
+
+  const user = new User({ fullName, email, password });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registeration Successful",
+  });
 });
 
-app.get("/", (req, res) => {
-  res.json({ data: "Hello World" });
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.json({ message: "Email is required" });
+  }
+
+  if (!password) {
+    return res.json({ message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.json({ message: "User does not exist" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.json({
+      error: true,
+      message: "Email or Password is incorrect",
+    });
+  }
 });
 
 app.listen("8000");
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = new Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  createdOn: { type: Date, default: new Date.getTime() },
-});
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
