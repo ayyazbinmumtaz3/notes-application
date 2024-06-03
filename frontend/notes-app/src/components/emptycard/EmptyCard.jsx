1bb738060c5c9347a02b6a8fc0ce3569661b1cfe
Ayyaz Bin Mumtaz <ayyazbinmumtaz3@yahoo.com>
2024-06-03 22:34:59 +0500

    add new icon image for empty page

--- frontend/notes-app/src/components/emptycard/EmptyCard.jsx
+++ frontend/notes-app/src/components/emptycard/EmptyCard.jsx
@@ -1,7 +1,7 @@
 const EmptyCard = ({ imgSrc, message }) => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
-      <img src={imgSrc} alt="Add a note" className="w-[200px]" />
+      <img src={imgSrc} alt="Add a note" className="w-[150px]" />
       <p className="w-1/2 text-md font-medium text-slate-700 text-center leading-7 mt-10">
         {message}
       </p>
